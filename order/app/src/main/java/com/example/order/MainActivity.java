adc253375605fa4c09c4d57a5fcf7b49ad9f11a3
Disha Satpute <dishasatpute44@gmail.com>
2024-07-02 23:24:43 +0530

    checkbox action handling with toast

---
+++ order/app/src/main/java/com/example/order/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+
+
+public class MainActivity extends AppCompatActivity {
+    CheckBox pizza,coffe,burger;
+    Button buttonOrder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButtonClick();
+    }
+    public void addListenerOnButtonClick(){
+        pizza=(CheckBox)findViewById(R.id.checkBox);
+        coffe=(CheckBox)findViewById(R.id.checkBox2);
+        burger=(CheckBox)findViewById(R.id.checkBox3);
+        buttonOrder=(Button)findViewById(R.id.button);
+
+        buttonOrder.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                int totalamount=0;
+                StringBuilder result=new StringBuilder();
+                result.append("Selected Items:");
+                if(pizza.isChecked()){
+                    result.append("\nPizza 100Rs");
+                    totalamount+=100;
+                }
+                if(coffe.isChecked()){
+                    result.append("\nCoffe 50Rs");
+                    totalamount+=50;
+                }
+                if(burger.isChecked()){
+                    result.append("\nBurger 120Rs");
+                    totalamount+=120;
+                }
+                result.append("\nTotal: "+totalamount+"Rs");
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_layout));
+                TextView tv = (TextView) layout.findViewById(R.id.txt);
+                tv.setText(result.toString());
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER_VERTICAL, 100, 100);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
+
+            }
+
+        });
+
+
+
+    }
+}
